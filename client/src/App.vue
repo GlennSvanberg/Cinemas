cae1385d8d54b6ad9894a7a1c570b0c89e3df789
Glenn Svanberg <signeratsvanberg@gmail.com>
2020-05-15 13:44:41 +0200

    Load cinemas and show as cards

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center ml-6">
+        <v-spacer></v-spacer>
+        <h1>Biografer</h1>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container grid-list-lg>
+        <v-row>
+          <v-col v-for="cinema in cinemas" :key="cinema.id">
+            <v-card>
+              <v-card-title v-text="cinema.title" primary-title class="primary white--text" dark></v-card-title>
+              <v-card-text v-text="cinema.description" class="mt-4"></v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="success">Se Filmer</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  methods: {
+    fetchCinemas: function() {
+      const baseURI = "http://localhost:8080/api/cinema";
+      this.$http.get(baseURI).then(result => {
+        this.cinemas = result.data;
+      });
+    }
+  },
+  components: {},
+  created() {
+    this.fetchCinemas();
   },
 
   data: () => ({
-    //
-  }),
+    cinemas: []
+  })
 };
 </script>
